2a800786764ee5a14738a983d6b115751a6e2203
sampandit553 <sameertiwari737697@gmail.com>
2026-02-18 12:34:58 +0530

    n

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  build: {
+    // Increase chunk size warning limit if needed
+    chunkSizeWarningLimit: 1000, // in kB
+
+    rollupOptions: {
+      output: {
+        // Split vendor libraries into a separate chunk
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
