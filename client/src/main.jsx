4aceb8cab89475659b64596ab84197eb5597acb1
sampandit553 <sameertiwari737697@gmail.com>
2026-02-28 08:51:25 +0530

    n

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
- <HashRouterRouter>
+ <HashRouter>
     <App />
-  </HashRouterRouter>,
+  </HashRouter>,
 )
